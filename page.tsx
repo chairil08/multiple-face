4e035301c7d4cab05e7484d194362d26a7654abe
chairil08 <chairilcibodas@gmail.com>
2025-04-16 09:43:57 +0700

    Delete page.tsx

--- page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold">Multiple Face AI - Coming Soon!</h1>
-    </div>
-  );
-}
